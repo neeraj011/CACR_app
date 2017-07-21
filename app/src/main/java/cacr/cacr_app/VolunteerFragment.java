dc21bdc74f7eb98682a52bbee0d53eab3e924db5
juhisb <juhisbhagtani@gmail.com>
2017-07-21 20:47:01 +0530

    Retrieve Vol

--- app/src/main/java/cacr/cacr_app/VolunteerFragment.java
+++ app/src/main/java/cacr/cacr_app/VolunteerFragment.java
@@ -15,14 +15,19 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VolunteerFragment extends Fragment implements AdapterView.OnItemSelectedListener{
     Spinner s1,s2;
@@ -39,6 +44,8 @@ public class VolunteerFragment extends Fragment implements AdapterView.OnItemSel
     public static String url = "https://neerajjethnani01.000webhostapp.com/cacr/get_institutions.php";
     public static String url1 = "https://neerajjethnani01.000webhostapp.com/cacr/get_locations.php";
     public static String url2 = "https://neerajjethnani01.000webhostapp.com/cacr/get_all_volunteers.php";
+    public static String url3 = "https://neerajjethnani01.000webhostapp.com/cacr/get_institutions_data.php";
+    public static String url4 = "https://neerajjethnani01.000webhostapp.com/cacr/get_locations_data.php";
     public VolunteerFragment() {
         // Required empty public constructor
     }
@@ -69,32 +76,48 @@ public class VolunteerFragment extends Fragment implements AdapterView.OnItemSel
         // attaching data adapter to spinner
         s1.setAdapter(dataAdapter);
         s1.setOnItemSelectedListener(this);
-
+        s2.setOnItemSelectedListener(this);
         return view;
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
-        switch (position){
-            case 0:
-                s2.setVisibility(View.GONE);
-                list1.setVisibility(View.VISIBLE);
-                list2.setVisibility(View.GONE);
-                new RetrieveAll().execute();
-                break;
-            case 1:
-                s2.setVisibility(View.VISIBLE);
-                new RetrieveDon().execute();
-                list2.setVisibility(View.VISIBLE);
-                list1.setVisibility(View.GONE);
-                break;
-            case 2:
-                s2.setVisibility(View.VISIBLE);
-                new RetrieveInst().execute();
-                list2.setVisibility(View.VISIBLE);
-                list1.setVisibility(View.GONE);
-                break;
+        Spinner spinner=(Spinner)parent;
+        if (spinner.getId()==R.id.spinner1) {
+            switch (position) {
+                case 0:
+                    s2.setVisibility(View.INVISIBLE);
+                    list1.setVisibility(View.VISIBLE);
+                    list2.setVisibility(View.GONE);
+                    new RetrieveAll().execute();
+                    break;
+                case 1:
+                    s2.setVisibility(View.VISIBLE);
+                    new RetrieveDon().execute();
+                    list1.setVisibility(View.VISIBLE);
+                    list2.setVisibility(View.GONE);
+                    break;
+                case 2:
+                    s2.setVisibility(View.VISIBLE);
+                    new RetrieveInst().execute();
+                    list1.setVisibility(View.VISIBLE);
+                    list2.setVisibility(View.GONE);
+                    break;
 
+            }
+        }
+        else if(spinner.getId()==R.id.spinner2)
+        {
+            if(s1.getSelectedItemPosition()==1)
+            {
+                location=s2.getSelectedItem().toString();
+                new RetrieveOnLoc().execute();
+            }
+            else if(s1.getSelectedItemPosition()==2)
+            {
+                institute = s2.getSelectedItem().toString();
+                new RetrieveOnInst().execute();
+            }
         }
     }
 
@@ -231,6 +254,166 @@ public class VolunteerFragment extends Fragment implements AdapterView.OnItemSel
             try{
                 HttpClient httpClient = new DefaultHttpClient();
                 HttpPost httpPost = new HttpPost(url2);
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+            }catch (Exception e){
+                // Toast.makeText(getApplicationContext(), exceptionMessage+", Ex1", Toast.LENGTH_SHORT).show();
+            }try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                while((line=reader.readLine())!=null){
+                    sb.append(line+"\n");
+                }
+                result = sb.toString();
+                System.out.println("-----JSON Data-----");
+                System.out.println(result);
+                is.close();
+            }catch(Exception e){
+                //Toast.makeText(getApplicationContext(), exceptionMessage+", Ex2", Toast.LENGTH_SHORT).show();
+            }try{
+                combinedText="";
+                JSONArray jsonArray = new JSONArray(result);
+                int totalCount = jsonArray.length();
+                for(int i=0; i<totalCount; i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    name_arr += jsonObject.getString("name")+":";
+                    email_arr +=jsonObject.getString("email")+":";
+                    contact_arr +=jsonObject.getString("contact")+":";
+                    institute_arr +=jsonObject.getString("institute")+":";
+                    location_arr +=jsonObject.getString("location")+":";
+                    combinedText += (i+1)+". Name '"+jsonObject.getString("name")+"', "+
+                            "Email '"+jsonObject.getString("email")+"',"+
+                            "Contact '"+jsonObject.getString("contact")+"',"+
+                            "Institution '"+jsonObject.getString("institute")+"',"+
+                            "Location '"+jsonObject.getString("location")+"',"+":";
+                }
+
+            }catch (Exception e){
+                //Toast.makeText(getApplicationContext(), exceptionMessage+", Ex3", Toast.LENGTH_SHORT).show();
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            name_arr1 = name_arr.split(":");
+            email_arr1 = email_arr.split(":");
+            contact_arr1 = contact_arr.split(":");
+            institute_arr1 = institute_arr.split(":");
+            location_arr1 = location_arr.split(":");
+            combinedArray = combinedText.split(":");
+
+            list1.setAdapter(new ArrayAdapter<String>(getContext(),
+                    android.R.layout.simple_list_item_1, combinedArray));
+
+
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(getContext());
+            progressDialog.setTitle("Loading...");
+            progressDialog.setMessage("Please Wait ... ");
+
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+    }
+    private class RetrieveOnInst extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try{
+
+                List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+                nameValuePairList.add(new BasicNameValuePair("institution", institute));
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(url3);
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairList));
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+            }catch (Exception e){
+                // Toast.makeText(getApplicationContext(), exceptionMessage+", Ex1", Toast.LENGTH_SHORT).show();
+            }try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                while((line=reader.readLine())!=null){
+                    sb.append(line+"\n");
+                }
+                result = sb.toString();
+                System.out.println("-----JSON Data-----");
+                System.out.println(result);
+                is.close();
+            }catch(Exception e){
+                //Toast.makeText(getApplicationContext(), exceptionMessage+", Ex2", Toast.LENGTH_SHORT).show();
+            }try{
+                combinedText="";
+                JSONArray jsonArray = new JSONArray(result);
+                int totalCount = jsonArray.length();
+                for(int i=0; i<totalCount; i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    name_arr += jsonObject.getString("name")+":";
+                    email_arr +=jsonObject.getString("email")+":";
+                    contact_arr +=jsonObject.getString("contact")+":";
+                    institute_arr +=jsonObject.getString("institute")+":";
+                    location_arr +=jsonObject.getString("location")+":";
+                    combinedText += (i+1)+". Name '"+jsonObject.getString("name")+"', "+
+                            "Email '"+jsonObject.getString("email")+"',"+
+                            "Contact '"+jsonObject.getString("contact")+"',"+
+                            "Institution '"+jsonObject.getString("institute")+"',"+
+                            "Location '"+jsonObject.getString("location")+"',"+":";
+                }
+
+            }catch (Exception e){
+                //Toast.makeText(getApplicationContext(), exceptionMessage+", Ex3", Toast.LENGTH_SHORT).show();
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            name_arr1 = name_arr.split(":");
+            email_arr1 = email_arr.split(":");
+            contact_arr1 = contact_arr.split(":");
+            institute_arr1 = institute_arr.split(":");
+            location_arr1 = location_arr.split(":");
+            combinedArray = combinedText.split(":");
+
+            list1.setAdapter(new ArrayAdapter<String>(getContext(),
+                    android.R.layout.simple_list_item_1, combinedArray));
+
+
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(getContext());
+            progressDialog.setTitle("Loading...");
+            progressDialog.setMessage("Please Wait ... ");
+
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+    }
+    private class RetrieveOnLoc extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try{
+
+                List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+                nameValuePairList.add(new BasicNameValuePair("location", location));
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(url4);
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairList));
                 HttpResponse httpResponse = httpClient.execute(httpPost);
                 HttpEntity httpEntity = httpResponse.getEntity();
                 is = httpEntity.getContent();
